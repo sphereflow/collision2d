4636ba9c1feb5b91e992989a80dabaab9067e3c4
sphereflow <johannprescher@gmx.de>
2020-10-05 20:19:35 +0200

    Remove Impl Scale for V2

--- src/geo/traits.rs
+++ src/geo/traits.rs
@@ -60,14 +60,6 @@ impl Scale for P2 {
     }
 }
 
-impl Scale for V2 {
-    fn scale(&mut self, scale_x: Float, scale_y: Float) {
-        self.x *= scale_x;
-        self.y *= scale_y;
-    }
-    fn scale_position(&mut self, _scale_x: Float, _scale_y: Float) {}
-}
-
 pub trait Intersect<T> {
     type Intersection;
 
